69637939fb292617feaf725e7a3e0832f17d5b29
André Levi <levi.silva@rentcars.com>
2023-06-01 20:45:21 -0300

    Initial Commit

---
+++ ShopUi/ShopUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopUiApp.swift
+//  ShopUi
+//
+//  Created by André Levi Oliveira Silva on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ShopUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
